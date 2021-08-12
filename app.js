5dc09758ff12c6c29b559e48ce470ea7ed7437fa
Mani <manidev501@gmail.com>
2021-08-12 10:57:12 +0000

    get ouput error is came

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+const path = require("path");
+
+const app = express();
+
+const dbPath = path.join(__dirname, "cricketTeam.db");
+
+let db = null;
+
+const initializeDbAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3000, () => {
+      console.log("Server Running at http://localhost:3000/");
+    });
+  } catch (e) {
+    console.log(`DB Error:${e.message}`);
+    process.exit(1);
+  }
+};
+
+initializeDbAndServer();
+
+app.get("/players/", async (request, response) => {
+  const getCricket_teamQuery = `
+    SELECT * FROM cricket_team ORDER BY player_id; 
+    `;
+  const playersArray = await db.all(getPlayersQuery);
+  response.send(playersArray);
+});
